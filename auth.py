8a19c187d1785eb008c14f2ca6ad349a40bb8694
Tanner Oates <61767694+TannerOates2@users.noreply.github.com>
2020-09-11 14:54:02 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
-consumer_key        = 'FwpowsIzbJFpeuWX6krlOrPAk'
-consumer_secret     = '30Jh2MYaoH4MOLTOe372cxH98HCgucIfkiBfaJ13f1T9bikBsb'
-access_token        = '1248583243693043712-8cwClZEoJuWNr7M8rNEKBuebAsz7f9'
-access_token_secret = 'OV2Ph4dXYSamvDcsK7pnNdKQJhytT9JehG6wdzWa0viAT'
+#Put in your account access codes
+consumer_key        = ''
+consumer_secret     = ''
+access_token        = ''
+access_token_secret = ''
